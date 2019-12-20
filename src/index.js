afabeb28ded5092359f6834e2122d89d0f26da8d
Damon Skelhorn <skelhd@skels.lan>
2019-12-20 22:05:25 +0000

    removed page.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'bootstrap/scss/bootstrap.scss';
 import './styles.scss';
-import './page';
 import App from './components/App'
 import { owmBuild } from './version';
 
