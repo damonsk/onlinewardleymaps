bf15a1a01c7a041c7bc802740447729866524cd3
Damon Skelhorn <skelhd@skels.lan>
2019-12-20 22:21:11 +0000

    remove twitter

--- src/components/editor/Breadcrumb.js
+++ src/components/editor/Breadcrumb.js
@@ -4,11 +4,7 @@ const Breadcrumb = (props) => (
 
     <nav aria-label="breadcrumb">
         <ol className="breadcrumb">
-            <li className="breadcrumb-item">Your Map URL <a href="#" id="url">{props.currentUrl}</a><span className="btn"><a href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-                        className="twitter-share-button" data-via="damonsk" data-hashtags="WardleyMaps" data-related=""
-                        data-show-count="false">Tweet</a>
-                    <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script></span>
-            </li>
+            <li className="breadcrumb-item">Your Map URL <a href="#" id="url">{props.currentUrl}</a></li>
         </ol>
     </nav>
 
