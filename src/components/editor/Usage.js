fdfca1203d67d4132c5976221e2eb7c31fb40641
Damon Skelhorn <skelhd@skels.lan>
2019-12-22 21:28:00 +0000

    moved into react components.

--- src/components/editor/Usage.js
+++ src/components/editor/Usage.js
@@ -1,79 +1,124 @@
 import React from 'react';
+var createReactClass = require('create-react-class');
 
 function Usage(props){
 
-    const addOnClick = (event) => {
+    const addOnClick = (txt) => {
         let before = props.mapText;
         console.log(before);
-        before = before + "\n" + event.target.textContent.trim();
+        before = before + (props.mapText.trim().length > 0 ? "\n" : "") + txt.trim();
         props.mutateMapText(before);
     }
     
     return (
     <p className="small">Usage:<br /><br />
-        <strong>To create a component:</strong> <br />
-        component Name [Visibility (Y Axis), Maturity (X Axis)]
-        <br /><br />
-        <strong>Example:</strong> <br /><a onClick={event => addOnClick(event)} href="#" className="add">component Customer [1, 0.5]</a><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">component Cup of Tea [0.9, 0.5]</a>
-        <br /><br />
-        ------------------------<br />
-        <strong>To evolve a component:</strong> <br />
-        component Name [Visibility (Y Axis), Maturity (X Axis)] evole (X Axis)
-        <br /><br />
-        <strong>Example:</strong> <br /><a onClick={event => addOnClick(event)} href="#" className="add">component Customer [1, 0.5] evolve 0.8</a><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">component Cup of Tea [0.9, 0.5] evolve 0.8</a>
-        <br /><br />
-        ------------------------<br />
-        <strong>To evolve a component with inertia:</strong> <br />
-        component Name [Visibility (Y Axis), Maturity (X Axis)] evole (X Axis) inertia
-        <br /><br />
-        <strong>Example:</strong> <br /><a onClick={event => addOnClick(event)} href="#" className="add">component Customer [1, 0.5] evolve 0.8 inertia</a><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">component Cup of Tea [0.9, 0.5] evolve 0.8 inertia</a>
-        <br /><br />
-        ------------------------<br />
-        <strong>To link components:</strong><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">Start Component->End Component</a>
-        <br /><br />
-        <strong>Example:</strong><br /> <a onClick={event => addOnClick(event)} href="#" className="add">Customer->Cup of Tea</a>
-        <br /><br />
-        ------------------------<br />
-        <strong>To indicate flow:</strong><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">Start Component+&lt;&gt;End Component</a>
-        <br /><br />
-        <strong>Example:</strong><br /> <a onClick={event => addOnClick(event)} href="#" className="add">Public+&lt;&gt;Cup of Tea</a>
-        <br /><br />
-        ------------------------<br />
-        <strong>To indicate flow - past components only:</strong><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">Start Component+&lt;End Component</a>
-        <br /><br />
-        <strong>Example:</strong><br /> <a onClick={event => addOnClick(event)} href="#" className="add">Hot Water+&lt;Kettle</a>
-        <br /><br />
-        ------------------------<br />
-        <strong>To indicate flow - future components only:</strong><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">Start Component+&gt;End Component</a>
-        <br /><br />
-        <strong>Example:</strong><br /> <a onClick={event => addOnClick(event)} href="#" className="add">Hot Water+&gt;Kettle</a>
-        <br /><br />
-        ------------------------<br />
-        <strong>To indicate flow - with label:</strong><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">Start Component+'insert text'&gt;End Component</a>
-        <br /><br />
-        <strong>Example:</strong><br /> <a onClick={event => addOnClick(event)} href="#" className="add">Hot Water+'+$0.10'&gt;Kettle</a>
-        <br /><br />
-        ------------------------<br />
-        <strong>Available styles:</strong><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">style wardley</a>
-        <br /><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">style handwritten</a>
-        <br /><br />
-        ------------------------<br />
-        <strong>To set evolution labels:</strong><br />
-        <a onClick={event => addOnClick(event)} href="#" className="add">evolution First->Second->Third->Fourth</a>
-        <br /><br />
-        <strong>Example:</strong><br /> <a onClick={event => addOnClick(event)} href="#" className="add">evolution Novel->Emerging->Good->Best</a>
+
+        <UsageDefinition 
+            title={"To create a component:"} 
+            summary={"component Name [Visibility (Y Axis), Maturity (X Axis)]"}
+            example={"component Customer [1, 0.5]"}
+            example2={"component Cup of Tea [0.9, 0.5]"}
+            addOnClick={addOnClick}
+            />
+
+        <UsageDefinition 
+            title={"To evolve a component:"} 
+            summary={"component Name [Visibility (Y Axis), Maturity (X Axis)] evole (X Axis)"}
+            example={"component Customer [1, 0.5] evolve 0.8"}
+            example2={"component Cup of Tea [0.9, 0.5] evolve 0.8"}
+            addOnClick={addOnClick}
+            />
+
+        <UsageDefinition 
+            title={"To evolve a component with inertia:"} 
+            summary={"component Name [Visibility (Y Axis), Maturity (X Axis)] evole (X Axis) inertia"}
+            example={"component Customer [1, 0.5] evolve 0.8 inertia"}
+            example2={"component Cup of Tea [0.9, 0.5] evolve 0.8 inertia"}
+            addOnClick={addOnClick}
+            />
+
+        <UsageDefinition 
+            title={"To link components:"} 
+            summary={""}
+            example={"Start Component->End Component"}
+            example2={"Customer->Cup of Tea"}
+            addOnClick={addOnClick}
+            />
+
+        <UsageDefinition 
+            title={"To indicate flow:"} 
+            summary={""}
+            example={"Start Component+<>End Component"}
+            example2={"Customer+<>Cup of Tea"}
+            addOnClick={addOnClick}
+            />
+
+        <UsageDefinition 
+            title={"To indicate flow - past components only:"} 
+            summary={""}
+            example={"Start Component+<End Component"}
+            example2={"Hot Water+<Kettle"}
+            addOnClick={addOnClick}
+            />
+
+        <UsageDefinition 
+            title={"To indicate flow - future components only:"} 
+            summary={""}
+            example={"Start Component+>End Component"}
+            example2={"Hot Water+>Kettle"}
+            addOnClick={addOnClick}
+            />
+        
+        <UsageDefinition 
+            title={"To indicate flow - with label:"} 
+            summary={""}
+            example={"Start Component+'insert text'>End Component"}
+            example2={"Hot Water+'$0.10'>Kettle"}
+            addOnClick={addOnClick}
+            />
+
+        <UsageDefinition 
+            title={"Stages of Evolution:"} 
+            summary={"Change the stages of evolution labels on the map"}
+            example={"evolution First->Second->Third->Fourth"}
+            example2={"evolution Novel->Emerging->Good->Best"}
+            addOnClick={addOnClick}
+            />
+
+        <UsageDefinition 
+            title={"Available styles:"} 
+            summary={"Change the look and feel of a map"}
+            example={"style wardley"}
+            example2={"style handwritten"}
+            addOnClick={addOnClick}
+            />
     </p>
     )
 }
+
+var UsageDefinition = createReactClass({
+    render: function() {
+        return (
+            <>
+            <strong>{this.props.title}</strong>
+            {this.props.summary.length > 0 ? <><br /> {this.props.summary} </>:null}
+            <br /><br />
+            <strong>Example:</strong><br />
+            <UsageExample addOnClick={this.props.addOnClick} example={this.props.example} />
+            {this.props.example2.length > 0 ? <><br /><UsageExample addOnClick={this.props.addOnClick} example={this.props.example2} /> </> : null}
+            <br /><br />
+            ------------------------<br />
+            </>
+        );
+    }
+});
+
+var UsageExample = createReactClass({
+    render: function() {
+        return (
+            <a onClick={() => this.props.addOnClick(this.props.example)} href="#" className="add">{this.props.example}</a>
+        );
+    }
+});
 
 export default Usage;
