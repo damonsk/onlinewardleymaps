0715d6030de7ea51a14793115a3073759892bbc2
Damon Skelhorn <skelhd@skels.lan>
2019-12-22 21:49:41 +0000

    introduced mapPosCalc.

---
+++ src/MapPositionCalculator.js
@@ -0,0 +1,11 @@
+export default class MapPositionCalculator {
+
+    visibilityToY (visibility, mapHeight) {
+        return (1 - visibility) * mapHeight;
+    }
+    
+    maturityToX (maturity, mapWidth) {
+        return maturity * mapWidth;
+    }
+
+}
