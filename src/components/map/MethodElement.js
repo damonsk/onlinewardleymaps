7628a8a12ec372dc09818c241f78f9c74d9b9670
damonsk <dskelly16@gmail.com>
2019-12-23 11:19:35 +0000

    remove copy pasta from renderSwardley

--- src/components/map/MethodElement.js
+++ src/components/map/MethodElement.js
@@ -30,13 +30,4 @@ function MethodElement(props){
     )
 }
 
-//     
-
-//     var elementSvg =
-//         '<g id="method_' + element.id + '" transform="translate(' + x + ',' + y + ')">' +
-//         '<circle id="element_circle_' + element.id + '" cx="0" cy="0" r="20" stroke="' + (method.method == "outsource" ? '#444444' : (method.method == "build" ? "#000000" : '#D6D6D6')) + '" fill="' + (method.method == "outsource" ? '#444444' : (method.method == "build" ? "#D6D6D6" : '#AAA5A9')) + '" />' +
-//         '</g>';
-
-//     return elementSvg;
-
 export default Breadcrumb;
