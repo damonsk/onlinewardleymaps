508012581179186b8aabddde33bf246473907eab
damonsk <dskelly16@gmail.com>
2019-12-23 11:19:16 +0000

    defined MethodElement.

--- src/components/map/MethodElement.js
+++ src/components/map/MethodElement.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import MapPositionCalculator from "../../MapPositionCalculator";
 
 function MethodElement(props){
 
+    const mapCalc = new MapPositionCalculator();
+    const x = () => mapCalc.maturityToX(props.element.maturity, props.mapWidth);
+    const y = () => mapCalc.visibilityToY(props.element.visibility, props.mapHeight);
+
+    function defineStoke(){
+        switch(props.method.method){
+            case "outsourced": return "#444444";
+            case "build": return "#000000";
+            default: return "#D6D6D6";
+        }
+    }
+
+    function defineFill(){
+        switch(props.method.method){
+            case "outsourced": return "#444444";
+            case "build": return "#D6D6D6";
+            default: return "#AAA5A9";
+        }
+    }
+
     render()(
-        
+        <g id={"method_" + props.element.id} transform={"translate (" + x() + "," + y() +")"}>
+            <circle id={"element_circle_" + props.element.id} cx="0" cy="0" r="20" fill={defineFill()} stroke={defineStoke()} />
+        </g>
     )
 }
 
-//     var x = matToX(element.maturity, mapWidth);
-//     var y = visToY(element.visibility, mapHeight);
+//     
 
 //     var elementSvg =
 //         '<g id="method_' + element.id + '" transform="translate(' + x + ',' + y + ')">' +
