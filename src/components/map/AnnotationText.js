1eec4010fc93a60ee8d7f160552adae35f8f93af
damonsk <709078+damonsk@users.noreply.github.com>
2019-12-29 14:09:02 +0000

    remove onmove from annotation text

--- src/components/map/AnnotationText.js
+++ src/components/map/AnnotationText.js
@@ -8,8 +8,6 @@ function AnnotationText(props) {
 			textAnchor="start"
 			dy={props.parentIndex > 0 ? 18 : 0}
 			x={0}
-			onMouseDown={e => handleMouseDown(e)}
-			onMouseUp={e => handleMouseUp(e)}
 			fill="black"
 		>
 			&nbsp;{props.annotation.number}. {props.annotation.text}&nbsp;
