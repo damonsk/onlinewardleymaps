ac2860867df6c954304609d0441df015d75db5d2
damonsk <709078+damonsk@users.noreply.github.com>
2020-01-04 10:24:15 +0000

    Fixes #33 - multi line comments styling.

--- src/mode-owm.js
+++ src/mode-owm.js
@@ -4,6 +4,7 @@ define('ace/mode/owm', function(require, exports) {
 	var OwmHighlightRules = require('ace/mode/owm_highlight_rules')
 		.OwmHighlightRules;
 
+
 	var Mode = function() {
 		this.HighlightRules = OwmHighlightRules;
 	};
@@ -21,53 +22,107 @@ define('ace/mode/owm_highlight_rules', function(require, exports) {
 	var TextHighlightRules = require('ace/mode/text_highlight_rules')
 		.TextHighlightRules;
 
+	var DocCommentHighlightRules = function() {
+		this.$rules = {
+			"start" : [ {
+				token : "comment.doc.tag",
+				regex : "@[\\w\\d_]+" // TODO: fix email addresses
+			}, 
+			DocCommentHighlightRules.getTagRule(),
+			{
+				defaultToken : "comment.doc",
+				caseInsensitive: true
+			}]
+		};
+	};
+
+	oop.inherits(DocCommentHighlightRules, TextHighlightRules);
+	
+	DocCommentHighlightRules.getTagRule = function(start) {
+		return {
+			token : "comment.doc.tag.storage.type",
+			regex : "\\b(?:TODO|FIXME|XXX|HACK)\\b"
+		};
+	};
+	
+	DocCommentHighlightRules.getStartRule = function(start) {
+		return {
+			token : "comment.doc", // doc comment
+			regex : "\\/\\*(?=\\*)",
+			next  : start
+		};
+	};
+	
+	DocCommentHighlightRules.getEndRule = function (start) {
+		return {
+			token : "comment.doc", // closing comment
+			regex : "\\*\\/",
+			next  : start
+		};
+	};
+
 	var OwmHighlightRules = function() {
 		this.$rules = {
 			"start" : [
 				{
-				   "token" : ["keyword", "variable.parameter.function.asp"],
-				   "regex" : "(evolution|annotations|component|title|style|outsource|build|product|buy)(\\s*[a-zA-Z0-9\\s*]+)"
+					"token" : "punctuation.definition.comment.asp",
+					"regex" : "(\\/\\/.*$)"
 				},
+				 DocCommentHighlightRules.getStartRule("doc-start"),
 				{
-				   "token" : ["keyword", "variable.parameter.function.asp"],
-				   "regex" : "(annotation)(\\s*[a-zA-Z0-9\\s*]+)"
+					token : "comment", // multi line comment
+					regex : "\\/\\*",
+					next : "comment"
 				},
+				 {
+					"token" : ["keyword", "variable.parameter.function.asp"],
+					"regex" : "(evolution|annotations|annotation|component|title|style|outsource|build|product|buy)(\\s*[a-zA-Z0-9\\s*]+)"
+				 },
+				 {
+					"token" : "keyword",
+					"regex" : "(inertia)"
+				 },
+				 {
+					"token" : ["punctuation", "punctuation", "constant.numeric", "punctuation", "constant.numeric", "punctuation", "punctuation"],
+					"regex" : "(\\[*)(\\[)(\\d+(?:\\.\\d{1,})*)(\\,\\s*)(\\d+(?:\\.\\d{1,}))(\\])(\\]*)"
+				 },
+				 {
+					"token" : ["keyword", "constant.numeric"],
+					"regex" : "(evolve)(\\s*\\d+(?:\\.\\d{1,})*)"
+				 },
+				 {
+					"token" : ["keyword", "constant.numeric"],
+					"regex" : "(evolve)(\\s*(?:\\.\\d{1,})*)"
+				 },
+				 {
+					"token" : ["variable.parameter.function.asp", "punctuation", "variable.parameter.function.asp"],
+					"regex" : "(\\s*[a-zA-Z0-9\\s*]+)(\\-\\>)(\\s*[a-zA-Z0-9\\s*]+)"
+				 },
+				 {
+					"token" : ["variable.parameter.function.asp", "punctuation", "variable.parameter.function.asp"],
+					"regex" : "(\\s*[a-zA-Z0-9\\s*]+)(\\+(?:\\<\\>|\\<|\\>))(\\s*[a-zA-Z0-9\\s*]+)"
+				 },
+				 {
+					"token" : ["variable.parameter.function.asp", "punctuation", "punctuation.definition.string.begin.asp", "punctuation", "variable.parameter.function.asp"],
+					"regex" : "(\\s*[a-zA-Z0-9\\s*]+)(\\+(?:\\'))([^']+)(\\'(?:\\<\\>|\\<|\\>))(\\s*[a-zA-Z0-9\\s*]+)"
+				 },
+				 {
+					defaultToken : "text",
+				 }
+			 ],
+			 "comment" : [
 				{
-				   "token" : "keyword",
-				   "regex" : "(inertia)"
-				},
-				{
-				   "token" : ["punctuation", "punctuation", "constant.numeric", "punctuation", "constant.numeric", "punctuation", "punctuation"],
-				   "regex" : "(\\[*)(\\[)(\\d+(?:\\.\\d{1,})*)(\\,\\s*)(\\d+(?:\\.\\d{1,}))(\\])(\\]*)"
-				},
-				{
-				   "token" : ["keyword", "constant.numeric"],
-				   "regex" : "(evolve)(\\s*\\d+(?:\\.\\d{1,})*)"
-				},
-				{
-				   "token" : ["keyword", "constant.numeric"],
-				   "regex" : "(evolve)(\\s*(?:\\.\\d{1,})*)"
-				},
-				{
-				   "token" : ["variable.parameter.function.asp", "punctuation", "variable.parameter.function.asp"],
-				   "regex" : "(\\s*[a-zA-Z0-9\\s*]+)(\\-\\>)(\\s*[a-zA-Z0-9\\s*]+)"
-				},
-				{
-				   "token" : ["variable.parameter.function.asp", "punctuation", "variable.parameter.function.asp"],
-				   "regex" : "(\\s*[a-zA-Z0-9\\s*]+)(\\+(?:\\<\\>|\\<|\\>))(\\s*[a-zA-Z0-9\\s*]+)"
-				},
-				{
-				   "token" : ["variable.parameter.function.asp", "punctuation", "punctuation.definition.string.begin.asp", "punctuation", "variable.parameter.function.asp"],
-				   "regex" : "(\\s*[a-zA-Z0-9\\s*]+)(\\+(?:\\'))([^']+)(\\'(?:\\<\\>|\\<|\\>))(\\s*[a-zA-Z0-9\\s*]+)"
-				},
-				{
-				   defaultToken : "text",
+					token : "comment", // closing comment
+					regex : "\\*\\/",
+					next : "start"
+				}, {
+					defaultToken : "comment"
 				}
-			 ]
+			]
 		};
 	};
 
+	
 	oop.inherits(OwmHighlightRules, TextHighlightRules);
-
 	exports.OwmHighlightRules = OwmHighlightRules;
 });
