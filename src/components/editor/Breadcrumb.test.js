1c72f2e88e4b0c1ad68e357f495ee6c42bfffd91
Shaun Bulbrook <sbulbrook@worth.systems>
2020-01-05 19:06:15 +0000

    Added spec for Breadcrumb item 'your map'

--- src/components/editor/Breadcrumb.test.js
+++ src/components/editor/Breadcrumb.test.js
@@ -26,4 +26,13 @@ describe('Breadcrumb', () => {
 			});
 		});
 	});
+	describe('Unique Items', () => {
+		describe('Your Map', () => {
+			it.skip('should have the text prefix "Your Map"', () => {});
+			it.skip('should render the value of provided currentUrl', () => {});
+			it.skip('if provided currentUrl is "(unsaved)" anchor should link to an empty fragment', () => {});
+			it.skip('if valid link is provided the item should link to it.', () => {});
+			it.skip('should display "(unsaved)" in place of currentUrl value if junk data is provided', () => {});
+		});
+	});
 });
