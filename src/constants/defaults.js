3e39fcba6324326276fb5257d650b2cbb85736e0
damonsk <709078+damonsk@users.noreply.github.com>
2020-01-18 16:25:13 +0000

    moved var into const defaults file. reduce noise

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -25,3 +25,52 @@ export const MapDimensions = {
 	width: 500,
 	height: 600,
 };
+
+export const ExampleMap =
+	'title Tea Shop' +
+	'\r\n' +
+	'component Business [0.95, 0.63]' +
+	'\r\n' +
+	'component Public [0.95, 0.78]' +
+	'\r\n' +
+	'component Cup of Tea [0.79, 0.61]' +
+	'\r\n' +
+	'component Cup [0.73, 0.78]' +
+	'\r\n' +
+	'component Tea [0.63, 0.81]' +
+	'\r\n' +
+	'component Hot Water [0.52, 0.80]' +
+	'\r\n' +
+	'component Water [0.38, 0.82]' +
+	'\r\n' +
+	'component Kettle [0.43, 0.35] evolve 0.62' +
+	'\r\n' +
+	'component Power [0.1, 0.7] evolve 0.89' +
+	'\r\n' +
+	'Business->Cup of Tea' +
+	'\r\n' +
+	'Public->Cup of Tea' +
+	'\r\n' +
+	'Cup of Tea->Cup' +
+	'\r\n' +
+	'Cup of Tea->Tea' +
+	'\r\n' +
+	'Cup of Tea->Hot Water' +
+	'\r\n' +
+	'Hot Water->Water' +
+	'\r\n' +
+	'Hot Water->Kettle ' +
+	'\r\n' +
+	'Kettle->Power' +
+	'\r\n' +
+	'' +
+	'\r\n' +
+	'annotation 1 [[0.43,0.49],[0.08,0.79]] Standardising power allows Kettles to evolve faster' +
+	'\r\n' +
+	'annotation 2 [0.48, 0.85] Hot water is obvious and well known' +
+	'\r\n' +
+	'annotations [0.97, 0.02]' +
+	'\r\n' +
+	'' +
+	'\r\n' +
+	'style colour';
