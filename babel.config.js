32177c8676f52e86c15f16a0d15e42e12a00a86a
ChristianDavis <christian@csgnetwork.com>
2020-05-07 18:14:34 -0500

    return babel config for tests

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+const presets = [
+	// [
+	//   '@babel/env',
+	//   {
+	//     targets: {
+	//       edge: '17',
+	//       firefox: '60',
+	//       chrome: '67',
+	//       safari: '11.1',
+	//     },
+	//     useBuiltIns: 'usage',
+	//   }
+	// ]
+	'react-app',
+];
+const env = {
+	test: {
+		plugins: ['require-context-hook'],
+	},
+};
+module.exports = { presets, env };
