b2975a6061be7d3f7bf3f563121c7007271e137c
damonsk <709078+damonsk@users.noreply.github.com>
2020-05-08 11:09:43 +0100

    removed redundant passing in of func

--- src/components/map/Anchor.test.js
+++ src/components/map/Anchor.test.js
@@ -4,24 +4,24 @@ import { create } from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import * as MapStyles from '../../constants/mapstyles';
 
-const CreateAnchor = mutateCallback => (
+const CreateAnchor = () => (
 	<Anchor
 		anchor={{ name: 'AnchorName', maturity: 0.12, visibility: 0.67, id: 1 }}
 		mapStyleDefs={MapStyles.Plain}
 		mapDimensions={{ width: 600, height: 400 }}
 		mapText={''}
-		mutateMapText={mutateCallback}
+		mutateMapText={() => {}}
 	/>
 );
 
 describe('Anchor', () => {
 	describe('init', () => {
 		it('should render as a svg element', () => {
-			const component = create(CreateAnchor(() => {})).toJSON();
+			const component = create(CreateAnchor()).toJSON();
 			expect(component.type).toBe('g');
 		});
 		it('should contain text element with name set', () => {
-			const component = render(CreateAnchor(() => {}));
+			const component = render(CreateAnchor());
 			component.getAllByTestId('anchor_text_1').map(element => {
 				expect(element).toHaveClass('label');
 				expect(element).toContainHTML('AnchorName');
