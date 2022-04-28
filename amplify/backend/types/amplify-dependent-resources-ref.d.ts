7af4d0cf19779d11e36e1a01df8286fae44d1976
dls on steamdeck <dskelly16@gmail.com>
2022-04-28 13:21:31 +0100

    added amplify src

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "owm": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "owm48e969c9": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s316e6c341": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
---
+++ frontend/src/lib/amplify/awsConfig.ts
@@ -0,0 +1,16 @@
+import Amplify from 'aws-amplify';
+import config from '../../aws-exports'
+
+export const awsConfig = {
+  ...config,
+  oauth: {
+    ...config.oauth,
+  },
+} 
+
+export const configureAmplify = () => {
+  Amplify.configure({
+    ...awsConfig,
+    ssr: true,
+  })
+}
