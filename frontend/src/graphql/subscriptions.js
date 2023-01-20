fa48f3028863695a9c3b2a0c94da17cee080923d
dskelly16@gmail.com <dskelly16@gmail.com>
2023-01-20 21:38:43 +0000

    building and running

--- frontend/src/graphql/subscriptions.js
+++ frontend/src/graphql/subscriptions.js
@@ -2,131 +2,155 @@
 // this is an auto generated file. This will be overwritten
 
 export const onCreateUnauthenticatedMap = /* GraphQL */ `
-  subscription OnCreateUnauthenticatedMap {
-    onCreateUnauthenticatedMap {
-      id
-      mapText
-      name
-      imageData
-      mapIterations
-      createdAt
-      updatedAt
-    }
-  }
+	subscription OnCreateUnauthenticatedMap(
+		$filter: ModelSubscriptionUnauthenticatedMapFilterInput
+	) {
+		onCreateUnauthenticatedMap(filter: $filter) {
+			id
+			mapText
+			name
+			imageData
+			mapIterations
+			createdAt
+			updatedAt
+		}
+	}
 `;
 export const onUpdateUnauthenticatedMap = /* GraphQL */ `
-  subscription OnUpdateUnauthenticatedMap {
-    onUpdateUnauthenticatedMap {
-      id
-      mapText
-      name
-      imageData
-      mapIterations
-      createdAt
-      updatedAt
-    }
-  }
+	subscription OnUpdateUnauthenticatedMap(
+		$filter: ModelSubscriptionUnauthenticatedMapFilterInput
+	) {
+		onUpdateUnauthenticatedMap(filter: $filter) {
+			id
+			mapText
+			name
+			imageData
+			mapIterations
+			createdAt
+			updatedAt
+		}
+	}
 `;
 export const onDeleteUnauthenticatedMap = /* GraphQL */ `
-  subscription OnDeleteUnauthenticatedMap {
-    onDeleteUnauthenticatedMap {
-      id
-      mapText
-      name
-      imageData
-      mapIterations
-      createdAt
-      updatedAt
-    }
-  }
+	subscription OnDeleteUnauthenticatedMap(
+		$filter: ModelSubscriptionUnauthenticatedMapFilterInput
+	) {
+		onDeleteUnauthenticatedMap(filter: $filter) {
+			id
+			mapText
+			name
+			imageData
+			mapIterations
+			createdAt
+			updatedAt
+		}
+	}
 `;
 export const onCreatePublicMap = /* GraphQL */ `
-  subscription OnCreatePublicMap($owner: String) {
-    onCreatePublicMap(owner: $owner) {
-      id
-      mapText
-      readOnly
-      name
-      imageData
-      mapIterations
-      createdAt
-      updatedAt
-      owner
-    }
-  }
+	subscription OnCreatePublicMap(
+		$filter: ModelSubscriptionPublicMapFilterInput
+		$owner: String
+	) {
+		onCreatePublicMap(filter: $filter, owner: $owner) {
+			id
+			mapText
+			readOnly
+			name
+			imageData
+			mapIterations
+			createdAt
+			updatedAt
+			owner
+		}
+	}
 `;
 export const onUpdatePublicMap = /* GraphQL */ `
-  subscription OnUpdatePublicMap($owner: String) {
-    onUpdatePublicMap(owner: $owner) {
-      id
-      mapText
-      readOnly
-      name
-      imageData
-      mapIterations
-      createdAt
-      updatedAt
-      owner
-    }
-  }
+	subscription OnUpdatePublicMap(
+		$filter: ModelSubscriptionPublicMapFilterInput
+		$owner: String
+	) {
+		onUpdatePublicMap(filter: $filter, owner: $owner) {
+			id
+			mapText
+			readOnly
+			name
+			imageData
+			mapIterations
+			createdAt
+			updatedAt
+			owner
+		}
+	}
 `;
 export const onDeletePublicMap = /* GraphQL */ `
-  subscription OnDeletePublicMap($owner: String) {
-    onDeletePublicMap(owner: $owner) {
-      id
-      mapText
-      readOnly
-      name
-      imageData
-      mapIterations
-      createdAt
-      updatedAt
-      owner
-    }
-  }
+	subscription OnDeletePublicMap(
+		$filter: ModelSubscriptionPublicMapFilterInput
+		$owner: String
+	) {
+		onDeletePublicMap(filter: $filter, owner: $owner) {
+			id
+			mapText
+			readOnly
+			name
+			imageData
+			mapIterations
+			createdAt
+			updatedAt
+			owner
+		}
+	}
 `;
 export const onCreateMap = /* GraphQL */ `
-  subscription OnCreateMap($owner: String) {
-    onCreateMap(owner: $owner) {
-      id
-      mapText
-      private
-      name
-      imageData
-      mapIterations
-      createdAt
-      updatedAt
-      owner
-    }
-  }
+	subscription OnCreateMap(
+		$filter: ModelSubscriptionMapFilterInput
+		$owner: String
+	) {
+		onCreateMap(filter: $filter, owner: $owner) {
+			id
+			mapText
+			private
+			name
+			imageData
+			mapIterations
+			createdAt
+			updatedAt
+			owner
+		}
+	}
 `;
 export const onUpdateMap = /* GraphQL */ `
-  subscription OnUpdateMap($owner: String) {
-    onUpdateMap(owner: $owner) {
-      id
-      mapText
-      private
-      name
-      imageData
-      mapIterations
-      createdAt
-      updatedAt
-      owner
-    }
-  }
+	subscription OnUpdateMap(
+		$filter: ModelSubscriptionMapFilterInput
+		$owner: String
+	) {
+		onUpdateMap(filter: $filter, owner: $owner) {
+			id
+			mapText
+			private
+			name
+			imageData
+			mapIterations
+			createdAt
+			updatedAt
+			owner
+		}
+	}
 `;
 export const onDeleteMap = /* GraphQL */ `
-  subscription OnDeleteMap($owner: String) {
-    onDeleteMap(owner: $owner) {
-      id
-      mapText
-      private
-      name
-      imageData
-      mapIterations
-      createdAt
-      updatedAt
-      owner
-    }
-  }
+	subscription OnDeleteMap(
+		$filter: ModelSubscriptionMapFilterInput
+		$owner: String
+	) {
+		onDeleteMap(filter: $filter, owner: $owner) {
+			id
+			mapText
+			private
+			name
+			imageData
+			mapIterations
+			createdAt
+			updatedAt
+			owner
+		}
+	}
 `;
