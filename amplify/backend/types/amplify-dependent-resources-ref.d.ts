473f94790638a08ed7e2d667216da53d3d65b4b4
dskelly16@gmail.com <dskelly16@gmail.com>
2023-04-06 15:42:13 +0100

    amplify borked again

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,26 +1,26 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "owm": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "auth": {
-        "owm48e969c9": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "storage": {
-        "s316e6c341": {
-            "BucketName": "string",
-            "Region": "string"
-        }
+  "api": {
+    "owm": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
     }
+  },
+  "auth": {
+    "owm48e969c9": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s316e6c341": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
 }
