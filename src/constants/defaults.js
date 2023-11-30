773b793d40fe403bf84123f9cabb57347f210f83
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-30 15:53:41 +0000

    Update defaults.js

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -1,7 +1,7 @@
 export const PageTitle =
 	'Wardley Maps AI - Draw Wardley Maps in seconds using AI v2';
 
-export const ApiEndpoint = 'https://maps.wardleymaps.ai/v1/maps/';
+export const ApiEndpoint = ''https://api.maps.wardleymaps.ai/v1/maps/';/v1/maps/';
 
 export const defaultLabelOffset = { x: 5, y: -10 };
 export const increasedLabelOffset = { x: 5, y: -20 };
