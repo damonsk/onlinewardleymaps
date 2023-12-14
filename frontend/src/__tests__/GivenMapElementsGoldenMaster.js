f69825b0cca4c04d46dc58dfe76b248cf25ae396
ds <ds>
2023-12-14 14:20:56 +0000

    increasing test coverage of this part of the system as it's in a need of a refactor.  Using Golden Master approach

--- frontend/src/__tests__/GivenMapElementsGoldenMaster.js
+++ frontend/src/__tests__/GivenMapElementsGoldenMaster.js
@@ -9,31 +9,58 @@ function loadFileContent(fileName) {
 	return fs.readFileSync(filePath, 'utf-8');
 }
 
+// Reusable function to compare results
+function testResultEquality(result, fileName) {
+	const outputFileContent = loadFileContent(fileName);
+	//console.log(JSON.stringify(result));  // Uncomment for debugging
+	expect(JSON.stringify(result)).toBe(outputFileContent);
+}
+
 describe('So that large refactors can be done without breaking output of mapElements', function() {
 	test('When all possible map components are specified, ensure the output is as expected', function() {
 		const fileName = 'GoldenMasterMapText.txt';
 		const fileContent = loadFileContent(fileName);
 
-		const outputFileName = 'GoldenMasterConverterOutput.txt';
-		const outputFileContent = loadFileContent(outputFileName);
-
 		let result = new Converter().parse(fileContent);
-		//console.log(JSON.stringify(result));  //copy to GM.txt
-		expect(JSON.stringify(result)).toBe(outputFileContent);
+		testResultEquality(result, 'GoldenMasterConverterOutput.txt');
 
 		const mergeables = [{ collection: result.elements, type: 'component' }];
 		const me = new MapElements(mergeables, result.evolved, result.pipelines);
 
-		const mapElementsResult = me.getMergedElements();
-		//console.log(JSON.stringify(mapElementsResult));  //copy to GM.txt
-		const mergedFileName = 'GoldenMasterMapElementsMergedElements.txt';
-		const mergedFileContent = loadFileContent(mergedFileName);
-		expect(JSON.stringify(mapElementsResult)).toBe(mergedFileContent);
+		const testCases = [
+			{
+				fn: () => me.getMergedElements(),
+				fileName: 'GoldenMasterMapElementsMergedElements.txt',
+			},
+			{
+				fn: () => me.getMapPipelines(),
+				fileName: 'GoldenMasterMapElementsPipeline.txt',
+			},
+			{
+				fn: () => me.getEvolveElements(),
+				fileName: 'GoldenMasterMapElementsEvolve.txt',
+			},
+			{
+				fn: () => me.getEvolvedElements(),
+				fileName: 'GoldenMasterMapElementsEvolved.txt',
+			},
+			{
+				fn: () => me.getNonEvolvedElements(),
+				fileName: 'GoldenMasterMapElementsNonEvolved.txt',
+			},
+			{
+				fn: () => me.getNoneEvolvedOrEvolvingElements(),
+				fileName: 'GoldenMasterGetNoneEvolvedOrEvolvingElements.txt',
+			},
+			{
+				fn: () => me.getNoneEvolvingElements(),
+				fileName: 'GoldenMasterGetNoneEvolvingElements.txt',
+			},
+		];
 
-		const pipelineResult = me.getMapPipelines();
-		//console.log(JSON.stringify(pipelineResult));  //copy to GM.txt
-		const pipelineFileName = 'GoldenMasterMapElementsPipeline.txt';
-		const pipelineFileContent = loadFileContent(pipelineFileName);
-		expect(JSON.stringify(pipelineResult)).toBe(pipelineFileContent);
+		testCases.forEach(testCase => {
+			const { fn, fileName } = testCase;
+			testResultEquality(fn(), fileName);
+		});
 	});
 });
