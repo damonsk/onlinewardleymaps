957ac1c1c730a518707efb3c5057682f95d37267
ds <ds>
2023-12-15 11:26:09 +0000

    updated example map to include more usable elements.

--- frontend/src/constants/defaults.js
+++ frontend/src/constants/defaults.js
@@ -44,25 +44,32 @@ export const MapDimensions = {
 export const ExampleMap = `title Tea Shop
 anchor Business [0.95, 0.63]
 anchor Public [0.95, 0.78]
-component Cup of Tea [0.79, 0.61] label [19, -4]
+component Cup of Tea [0.79, 0.61] label [-76, 3]
 component Cup [0.73, 0.78]
 component Tea [0.63, 0.81]
 component Hot Water [0.52, 0.80]
 component Water [0.38, 0.82]
-component Kettle [0.43, 0.35] label [-57, 4]
+component Kettle [0.43, 0.35] label [-57, 4] (inertia)
 evolve Kettle 0.62 label [16, 7]
-component Power [0.1, 0.7] label [-27, 20]
-evolve Power 0.89 label [-12, 21]
+
+component Power [0.17, 0.79] label [-7, -15]
+pipeline Power
+{
+  component Nuclear [0.89] label [-17.00, 35.00]
+  component Solar [0.73] label [-20.00, 35.00]
+  component Coal [0.81] label [-13.00, 35.00]
+}
+
 Business->Cup of Tea
 Public->Cup of Tea
 Cup of Tea->Cup
 Cup of Tea->Tea
 Cup of Tea->Hot Water
 Hot Water->Water
-Hot Water->Kettle 
+Hot Water->Kettle; limited by 
 Kettle->Power
 
-annotation 1 [[0.43,0.49],[0.08,0.79]] Standardising power allows Kettles to evolve faster
+annotation 1 [[0.43,0.49],[0.21,0.73] ] Standardising power allows Kettles to evolve faster
 annotation 2 [0.48, 0.85] Hot water is obvious and well known
 annotations [0.60, 0.02]
 
