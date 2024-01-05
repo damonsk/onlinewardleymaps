ecd125848be0360b828bda76bf7d6fd8f0f457ce
ds <ds>
2024-01-05 18:23:37 +0000

    Remove redundant files from include to fix publish to NPM.

--- frontend/wmlandscape/src/index.js
+++ frontend/wmlandscape/src/index.js
@@ -56,6 +56,7 @@ import * as Defaults from '../../src/constants/defaults';
 import * as EditorPrefixes from '../../src/constants/editorPrefixes';
 import * as MapStyles from '../../src/constants/mapstyles';
 import * as Usages from '../../src/constants/usages';
+import * as FeatureSwitches from '../../src/constants/featureswitches';
 
 import AnchorExtractionStrategy from '../../src/conversion/AnchorExtractionStrategy';
 import AnnotationExtractionStrategy from '../../src/conversion/AnnotationExtractionStrategy';
@@ -72,7 +73,7 @@ import MethodExtractionStrategy from '../../src/conversion/MethodExtractionStrat
 import NoteExtractionStrategy from '../../src/conversion/NoteExtractionStrategy';
 import ParseError from '../../src/conversion/ParseError';
 import PipelineExtractionStrategy from '../../src/conversion/PipelineExtractionStrategy';
-import PipelineExtractionRunner from '../../src/conversion/PipelineExtractionRunner';
+import PipelineStrategyRunner from '../../src/conversion/PipelineStrategyRunner';
 import PresentationExtractionStrategy from '../../src/conversion/PresentationExtractionStrategy';
 import SubMapExtractionStrategy from '../../src/conversion/SubMapExtractionStrategy';
 import TitleExtractionStrategy from '../../src/conversion/TitleExtractionStrategy';
@@ -155,6 +156,7 @@ export {
 	EditorPrefixes,
 	MapStyles,
 	Usages,
+	FeatureSwitches,
 	AnchorExtractionStrategy,
 	AnnotationExtractionStrategy,
 	AttitudeExtractionStrategy,
@@ -170,7 +172,7 @@ export {
 	NoteExtractionStrategy,
 	ParseError,
 	PipelineExtractionStrategy,
-	PipelineExtractionRunner,
+	PipelineStrategyRunner,
 	PresentationExtractionStrategy,
 	SubMapExtractionStrategy,
 	TitleExtractionStrategy,
