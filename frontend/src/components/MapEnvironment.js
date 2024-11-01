8ba4f291b2535f59359e5552c04da759dea07807
damonsk <dskelly16@gmail.com>
2024-11-01 15:42:47 +0000

    export as svg to include xmlns to ensure proper presentation in other apps.

--- frontend/src/components/MapEnvironment.js
+++ frontend/src/components/MapEnvironment.js
@@ -318,7 +318,8 @@ function Environment(props) {
 	function downloadMapAsSVG() {
 		const svgMapText = mapRef.current
 			.getElementsByTagName('svg')[0]
-			.outerHTML.replace(/&nbsp;/g, ' ');
+			.outerHTML.replace(/&nbsp;/g, ' ')
+			.replace(/<svg([^>]*)>/, '<svg xmlns="http://www.w3.org/2000/svg"$1>');
 		saveMapText(
 			`<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">${svgMapText}`,
 			`${mapTitle}.svg`
