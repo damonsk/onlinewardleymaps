056d83127d232e37f147668235a4e65a54fbaeea
damonsk <dskelly16@gmail.com>
2024-11-03 15:31:00 +0000

    to typescript

---
+++ frontend/src/components/map/positionUpdaters/DefaultPositionUpdater.ts
@@ -0,0 +1,38 @@
+type MatcherFunction = (line: string, identifier: string, type: string) => boolean;
+type ActionFunction = (line: string, moved: any) => string;
+
+interface Replacer {
+    matcher: MatcherFunction;
+    action: ActionFunction;
+}
+
+export default class DefaultPositionUpdater {
+    type: string;
+    mapText: string;
+    mutator: (updatedText: string) => void;
+    replacers: Replacer[];
+
+    constructor(type: string, mapText: string, mutator: (updatedText: string) => void, replacers: Replacer[]) {
+        this.type = type;
+        this.mapText = mapText;
+        this.mutator = mutator;
+        this.replacers = replacers;
+    }
+
+    update(moved: any, identifier: string): void {
+        const updatedText = this.mapText
+            .split('\n')
+            .map(line => {
+                for (let i = 0; i < this.replacers.length; i++) {
+                    const r = this.replacers[i];
+                    if (r.matcher(line, identifier, this.type)) {
+                        return r.action(line, moved);
+                    }
+                }
+                return line;
+            })
+            .join('\n');
+
+        this.mutator(updatedText);
+    }
+}
