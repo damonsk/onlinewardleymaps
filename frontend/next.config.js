9bbc31abedd04a18ec5a1c1c8f0b22417272531b
damonsk <dskelly16@gmail.com>
2026-02-15 17:54:57 +0000

    update nextjs to 16

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,15 +7,8 @@ const nextConfig = {
     compiler: {
         styledComponents: true,
     },
-    webpack: (config, {isServer}) => {
-        // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
-            config.resolve.fallback = {
-                ...config.resolve.fallback,
-                fs: false,
-            };
-        }
-        return config;
+    turbopack: {
+        root: __dirname,
     },
 };
 
